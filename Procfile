d1f3a4134a94709aa3e4ba321cc00a8273fbc6a7
Nicolay <nicolay.krischenovich@gmail.com>
2020-06-30 22:11:30 +0300

    Deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn revcon.wsgi --log-file -
--- revcon/revcon/settings.py
+++ revcon/revcon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%uo&i0t058+nmoj0aj84(6+)gfq@*+d_^%ur=23=(*3(iz_+k0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -119,5 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+import dj_database_url
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
