00350a8063a4ec31428eb7e6d5eb0907aa3afacb
Nicolay <nicolay.krischenovich@gmail.com>
2020-07-01 00:19:03 +0300

    fix Deploy

--- revcon/revcon/settings.py
+++ revcon/revcon/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '%uo&i0t058+nmoj0aj84(6+)gfq@*+d_^%ur=23=(*3(iz_+k0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -42,12 +42,14 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
 ]
 
 ROOT_URLCONF = 'revcon.urls'
