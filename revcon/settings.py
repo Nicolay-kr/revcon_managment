c8f03499a73076cdf1a504021141133874334d5b
Nicolay-kr <nicolay.krischenovich@gmail.com>
2020-10-05 18:31:57 +0300

    fix: setting back

--- revcon/settings.py
+++ revcon/settings.py
@@ -79,11 +79,8 @@ WSGI_APPLICATION = 'revcon.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'revcondb',
-        'HOST': 'revconserver.postgres.database.azure.com',
-        'USER': 'revcon',
-        'PASSWORD': 'Banderas021290'                                        
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
