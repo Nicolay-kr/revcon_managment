92524f46e46bf173841461d7a48c13a95e168d0a
Nicolay-kr <55105304+Nicolay-kr@users.noreply.github.com>
2020-10-05 20:28:58 +0300

    Update settings.py

--- revcon/settings.py
+++ revcon/settings.py
@@ -79,7 +79,6 @@ WSGI_APPLICATION = 'revcon.wsgi.application'
 
 DATABASES = {
     'default': {
-<<<<<<< HEAD
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
